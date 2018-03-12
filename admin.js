caac46d9617e1c73e75c3f80e8032da282bbd9d4
Khalil <khalilnafis@gmail.com>
2018-03-11 20:26:26 -0400

    functionality for admin+browse, markup updates

---
+++ admin.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+	let products = JSON.parse(localStorage.getItem("products"));
+	if(!products){
+			products = [];
+		}
+	let productEntry = {
+		"productName": "",
+		"imageSrc":"",
+		"description":"",
+		"id":"",
+		"markup":""
+	};
+	$('#addProductBtn').on('click', function(event) {
+		event.preventDefault();
+
+		var productName = $("#productName").val();
+		var imageLink = $("#imageLink").val();
+		var description = $("#description").val();
+
+		addProduct(productName, imageLink, description);
+
+		var productName = $("#productName").val("");
+		var description = $("#description").val("");
+	});
+
+	function addProduct(productName, productImage, productDescription){
+		productEntry.productName = productName;
+		productEntry.imageSrc = productImage;
+		productEntry.description = productDescription;
+		productEntry.id = products.length+1;
+		productEntry.markup = "<div class=\"col-sm-4 text-center my-2\">" + 
+			"<div class=\"product-listing\"><img src="+ productEntry.imageSrc +">" +
+			"<p class=\"product-label\">"+productEntry.productName+"</p></div><button class=\"btn btn-primary\">Add to Cart</button></div>";
+		products.push(JSON.stringify(productEntry));
+	
+		localStorage.setItem("products", JSON.stringify(products));
+	};
+});
---
+++ browse.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	loadData();
+
+	function loadData(){
+		let products = JSON.parse(localStorage.getItem("products"));
+		if(products && products.length > 0){
+			for(let i=0; i<products.length; i++){
+				let product = JSON.parse(products[i]);
+				console.log(i);
+				$("#product-listings").append(product.markup);
+			}
+		}
+	}
+});
