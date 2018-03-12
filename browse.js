0251594ac6b6fe4c6103fc3a3ca6466c10605512
Khalil <khalilnafis@gmail.com>
2018-03-11 20:42:24 -0400

    cleanup

--- browse.js
+++ browse.js
@@ -6,7 +6,6 @@ $(document).ready(function() {
 		if(products && products.length > 0){
 			for(let i=0; i<products.length; i++){
 				let product = JSON.parse(products[i]);
-				console.log(i);
 				$("#product-listings").append(product.markup);
 			}
 		}
