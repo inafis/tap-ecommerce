a2978325865395941eb05a9b042e095e7b775b6a
Walton, Brandon [GTSUS] <bwalton5@its.jnj.com>
2018-03-12 15:00:51 -0400

    Updated Browse JS

--- browse.js
+++ browse.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
 	loadData();
 
-	function loadData(){
+	function loadData() {
 		let products = JSON.parse(localStorage.getItem("products"));
-		if(products && products.length > 0){
-			for(let i=0; i<products.length; i++){
-				let product = JSON.parse(products[i]);
+		if(products) {
+			products.forEach(function(productString) {
+				let product = JSON.parse(productString)
 				$("#product-listings").append(product.markup);
-			}
+			})
 		}
 	}
 });
